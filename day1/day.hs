1fc22f707177d4ee02e3a9efc4c02a98ba0b1b78
Meowyam <maryam@chilon.net>
2022-12-08 13:57:08 +0800

    day 1 part 2

--- day1/day.hs
+++ day1/day.hs
@@ -1,8 +1,12 @@
 import System.IO
 import Control.Monad
+import Data.List
 import Data.List.Split
+import Data.Ord
 
 main = do
   input <- readFile "input.txt"
   let elf = [ sum (map read x :: [Integer]) | x <- map lines $ splitOn "\n\n" input]
   print $ maximum elf
+  let topThree = sum $ take 3 $ sortOn Down elf
+  print topThree
